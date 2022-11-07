40b376ae451be47abb2ccb3525f9f1419bfdce64
Artem <varivonchikartem@gmail.com>
2022-11-07 19:04:18 +0300

    lesson 132

--- src/componenst/app/app.js
+++ src/componenst/app/app.js
@@ -7,6 +7,36 @@ import { AppFilter } from "../app-filter/app-filter";
 import { EmployeesList } from "../employees-list/employees-list";
 import { EmployeesAddForm } from "../employees-add-form/employees-add-form";
 
+const data = [
+    {
+        id: 1,
+        name: "Maxim P",
+        salary: 4800,
+        increase: false
+    },
+
+    {
+        id: 2,
+        name: "Viktor R",
+        salary: 3800,
+        increase: true
+    },
+
+    {
+        id: 3,
+        name: "Maxim S",
+        salary: 2800,
+        increase: false
+    },
+
+    {
+        id: 4,
+        name: "Artem V",
+        salary: 1800,
+        increase: false
+    },
+];
+
 const App = () => {
     return (
         <div className="app">
@@ -16,8 +46,8 @@ const App = () => {
                 <SearchPanel />
                 <AppFilter />
             </div>
-            <EmployeesList />
-            <EmployeesAddForm/>
+            <EmployeesList data = {data} />
+            <EmployeesAddForm />
         </div>
     );
 };
--- src/componenst/employees-list/employees-list.js
+++ src/componenst/employees-list/employees-list.js
@@ -1,16 +1,19 @@
 import React from "react";
-import './employees-list.css'
+import "./employees-list.css";
 import { EmployeesListItem } from "../employees-list-item/employees-list-item";
 
+const EmployeesList = ({ data }) => {
+    //creating our elements
+    const elements = data.map((item) => {
+        const { id, ...itemProps } = item;
 
-const EmployeesList = () => {
-    return(
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-        </ul>
-    )
-}
+        return (
+            //<EmployeesListItem name = {item.name} salary = {item.salary}/> 1 example
+            <EmployeesListItem key={id} {...itemProps} /> //2 example
+        );
+    });
 
-export { EmployeesList }
+    return <ul className="app-list list-group">{elements}</ul>;
+};
+
+export { EmployeesList };
